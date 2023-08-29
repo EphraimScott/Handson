a941b24a9b2a11f6a3c03a8a4cf7d0dce9c92dfe
Aluno <aluno@edu.senac.br>
2023-08-29 19:48:15 -0300

    Handson

---
+++ JS/handson02.js
@@ -0,0 +1,5 @@
+function exibirMensagem(){
+    var nome = document.getElementById ("nome").value;
+    var sobrenome = document.getElementById ("sobrenome").value;
+    alert(`Hello ${nome } ${sobrenome}`);
+}
---
+++ JS/handson03.js
@@ -0,0 +1,9 @@
+function somar(){
+    /*Entrada*/
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    /*processamento*/
+    var soma = Number.parseInt(num1)+Number.parseInt(num2);
+    /*saida*/
+    alert(num1+num2)
+}
