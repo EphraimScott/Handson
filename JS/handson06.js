6520452f513cf40d8585a9887948a9399e7e7350
Aluno <aluno@edu.senac.br>
2023-08-30 21:01:20 -0300

    handson 6 concluido

---
+++ JS/handson06.js
@@ -0,0 +1,9 @@
+function imparoupar() {
+    var verify = document.getElementById("verify").value;
+
+    if (verify%2 == 0) {
+        alert("PAAAAAAAARRRRR!!!!!!!!!!!!!!")
+    }else{
+        alert("IMPAAAAARRRRRR!!!!!!!!!!!!!!")
+    }
+}
---
+++ JS/handson07.js
@@ -0,0 +1,9 @@
+function verify(){
+    var aprfai = document.getElementById("nota").value;
+
+    if (aprfai >=3.7){
+        alert("APROVADO!!!!!")
+    }else{
+        alert("REPROVADO!!!!")
+    }
+}
